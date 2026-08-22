f2218b6a809abd6b2e3c14631620ae4c4b30c1de
momoodid <momoodid@outlook.com>
2026-08-23 01:21:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-#
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
-# 1.复制自定义DTS
 cp -f "${GITHUB_WORKSPACE}/adslr_g7.dts" target/linux/ramips/dts/
 
-# 2.注册新设备到 mt7621.mk
 cat >> target/linux/ramips/image/mt7621.mk << 'EOF'
 
 define Device/adslr_g7
@@ -20,9 +14,9 @@ define Device/adslr_g7
   KERNEL_LOADADDR := 0x80001000
   IMAGE_SIZE := 15040k
   DEVICE_CMDLINE := console=ttyS0,57600 root=/dev/mtdblock4 rootfstype=squashfs,jffs2
+  KERNEL := kernel-bin | append-dtb | lzma-loader | uImage none
+  KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma-loader | uImage none
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += adslr_g7
 EOF
-
-# 3.为设备 adslr_g7 启用 lzma-loader，修复 LZMA ERROR 1 问题
-sed -i '/^define Device\/adslr_g7$/,/^endef$/ s/^\([[:space:]]*\)DEVICE_VENDOR/\1$(Device\/uimage-lzma-loader)\n&/' target/linux/ramips/image/mt7621.mk
