b72075750ea6e570d65ee674a918e15de02f5ab8
momoodid <momoodid@outlook.com>
2026-09-13 02:33:04 +0800

    Modify ADSLR G7 device definition and packages
    Updated device definition for ADSLR G7 in mt7621.mk and modified related packages.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
+#
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
-cp -f "${GITHUB_WORKSPACE}/adslr_g7.dts" target/linux/ramips/dts/
+# 复制 DTS 到源码目录
+cp -f ./adslr_g7.dts target/linux/ramips/dts/
 
+# 追加 ADSLR G7 设备定义到 mt7621.mk
 cat >> target/linux/ramips/image/mt7621.mk << 'EOF'
 
 define Device/adslr_g7
   DEVICE_VENDOR := ADSLR
   DEVICE_MODEL := G7
   DEVICE_DTS := adslr_g7
-  DEVICE_PACKAGES := kmod-mt7603 kmod-mt7612e kmod-mt76x2
+  DEVICE_PACKAGES := kmod-mt7615e kmod-mt7615-firmware kmod-mt76-core kmod-mt76-connac
   SUPPORTED_DEVICES := adslr_g7
   USE_FIT := 0
   KERNEL_LOADADDR := 0x80001000
-  IMAGE_SIZE := 15040k
-  DEVICE_CMDLINE := console=ttyS0,57600 root=/dev/mtdblock4 rootfstype=squashfs,jffs2
   KERNEL := kernel-bin | append-dtb | lzma-loader | uImage none
   KERNEL_INITRAMFS := kernel-bin | append-dtb | lzma-loader | uImage none
   IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += adslr_g7
 EOF
-
-
-rm -rf build_dir/target-mipsel_24kc_musl/luci-base
